4b913132bce23a3660f560ad33986834982b10af
Arko Dalal <arka.dalal06@gmail.com>
2025-12-04 11:36:31 +0530

    Update Layout.js

--- Layout.js
+++ Layout.js
@@ -1,1 +1,15 @@
+import React from 'react';
+import Navbar from '@/components/layout/Navbar';
+import Footer from '@/components/layout/Footer';
 
+export default function Layout({ children }) {
+  return (
+    <div className="min-h-screen bg-[#0B1E47] flex flex-col">
+      <Navbar />
+      <main className="flex-1 pt-20">
+        {children}
+      </main>
+      <Footer />
+    </div>
+  );
+}
